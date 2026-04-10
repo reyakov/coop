6f0cefed33e0b980f434c5ff3009653033a2456a
Ren Amamiya <reya@lume.nu>
2026-04-10 09:32:04 +0700

    only process nip17 rumor

--- crates/chat/src/room.rs
+++ crates/chat/src/room.rs
@@ -400,6 +400,10 @@ impl Room {
                 .await?
                 .into_iter()
                 .filter_map(|event| UnsignedEvent::from_json(&event.content).ok())
+                .filter(|event| {
+                    // Filter out private direct messages and file messages
+                    event.kind != Kind::PrivateDirectMessage && event.kind != Kind::Custom(15)
+                })
                 .sorted_by_key(|message| message.created_at)
                 .collect();
 
